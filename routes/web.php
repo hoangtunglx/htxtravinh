31264b68533e38e9c93c885a30fd1cd60c67dda4
Le Anh <agulhanh@gmail.com>
2020-03-11 21:07:04 +0700

    Tạo master; dashboard: quản trị viên, cán bộ quản lý htx, cán bộ htx, nông dân

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,47 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+// Farmer - Nông dân
+Route::prefix('farmer')->name('farmer.')->group(function () {
+	Route::get('', function () {
+		return view('farmer.dashboard');
+	})->name('dashboard');
+
+	Route::prefix('loai-giong')->name('loaigiong.')->group(function () {
+		Route::get('', 'LoaiGiongController@getIndex')->name('index');
+		Route::get('create', 'LoaiGiongController@getCreate')->name('create');
+		Route::post('store', 'LoaiGiongController@postCreate')->name('store');
+		Route::get('edit/{id}', 'LoaiGiongController@getEdit')->name('edit');
+		Route::put('update', 'LoaiGiongController@postEdit')->name('update');
+		Route::delete('delete/{id}', 'LoaiGiongController@postDelete')->name('delete');
+	});
+});
+
+// Officer - Cán bộ hợp tác xã
+Route::prefix('officer')->name('officer.')->group(function () {
+	Route::get('', function () {
+		return view('officer.dashboard');
+	})->name('dashboard');
+});
+
+// Manager - Cán bộ quản lý hợp tác xã
+Route::prefix('manager')->name('manager.')->group(function () {
+	Route::get('', function () {
+		return view('manager.dashboard');
+	})->name('dashboard');
+});
+
+// Admin - Quản trị viên
+Route::prefix('admin')->name('admin.')->group(function () {
+	Route::get('', function () {
+		return view('admin.dashboard');
+	})->name('dashboard');
+});
+
+
+
+
 Auth::routes();
 
 Route::get('/', 'HomeController@index')->name('home');
