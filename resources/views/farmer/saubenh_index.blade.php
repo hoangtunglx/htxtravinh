5e102f8fc14f22f330cfedf04e4cf48bb3907c3e
huuphu0x99 <huuphuox99@gmail.com>
2020-04-01 08:18:50 +0700

    bo sung views

---
+++ resources/views/farmer/saubenh_index.blade.php
@@ -0,0 +1,61 @@
+
+
+@extends('farmer.layouts.master')
+
+@section('content')
+
+<div class="page-inner">
+  <header class="page-title-bar">
+    <nav aria-label="breadcrumb">
+      <ol class="breadcrumb">
+        <li class="breadcrumb-item active">
+          <a href=" {{ route('farmer.dashboard') }} "><i class="breadcrumb-icon fa fa-angle-left mr-2"></i>Dashboard</a>
+        </li>
+      </ol>
+    </nav>
+
+    <div class="d-md-flex align-items-md-start">
+      <h1 class="page-title mr-sm-auto"> Sâu bệnh </h1>
+      <div id="dt-buttons" class="btn-toolbar"></div>
+    </div>
+  </header>
+  <div class="page-section">
+    <div class="card card-fluid">
+      <div class="card-body">
+        @include('layouts.blocks.flash_message')
+        <table id="table-datatable-default" class="table">
+          <thead>
+            <tr>
+              <th class="text-center"> # </th>
+              <th> Tên sâu bệnh </th>
+        			<th> Đặc điểm </th>
+        			<th> Cây trồng tấn công </th>
+        			<th> Dấu hiệu </th>
+        			<th> Biện pháp phòng tránh </th>
+        			<th> Ghi chú </th>
+              <th style="width:100px; min-width:100px;"> &nbsp; </th>
+            </tr>
+          </thead>
+          <tbody>
+             @foreach($dsSauBenh ?? [] as $sauBenh)
+            <tr>
+              <td class="text-center"> {{ $loop->iteration }} </td>
+              <td> {{ $sauBenh->tensaubenh}} </td>
+          		<td> {{ $sauBenh->dacdiem }} </td>
+          		<td> {{ $sauBenh->caytrongtancong }} </td>
+          		<td> {{ $sauBenh->dauhieu}} </td>
+          		<td> {{ $sauBenh->bienphapphongtranh }} </td>
+          		<td> {{ $sauBenh->ghichu }} </td>
+              <td class="text-center">
+
+              </td>
+            </tr>
+            @endforeach
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</div>
+
+@endsection
---
+++ resources/views/officer/saubenh_edit.blade.php
@@ -0,0 +1,78 @@
+@extends('officer.layouts.master')
+
+@section('content')
+
+<div class="page-inner">
+  <header class="page-title-bar">
+    <nav aria-label="breadcrumb">
+      <ol class="breadcrumb">
+        <li class="breadcrumb-item active">
+          <a href=" {{ route('officer.dashboard') }} "><i class="breadcrumb-icon fa fa-angle-left mr-2"></i>Dashboard</a>
+          <a href=" {{ route('officer.saubenh.index') }} "><i class="breadcrumb-icon fa fa-angle-left mr-2"></i>Tiêu chuẩn sản xuất</a>
+        </li>
+      </ol>
+    </nav>
+    <div class="d-md-flex align-items-md-start">
+      <h1 class="page-title mr-sm-auto"> Tiêu chuẩn sản xuất <small> Chỉnh sửa </small> </h1>
+    </div>
+  </header>
+  <div class="page-section">
+    <div class="card card-fluid">
+      <div class="card-body">
+
+        @include('layouts.blocks.flash_message')
+
+        <form action=" {{ route('officer.saubenh.update') }} " method="post" >
+          @csrf()
+          @method('PUT')
+          <input type="hidden" name="saubenhid" value=" {{ $sauBenh->id }} ">
+          <fieldset>
+
+	
+            <div class="form-group">
+              <label for="tensaubenh">Tên sâu bệnh </label>
+              <textarea class="form-control @error('tensaubenh') is-invalid @enderror" id="tensaubenh" name="tensaubenh" rows="3" placeholder="tên sâu bệnh" autofocus>{{old('tensaubenh', $sauBenh->tensaubenh)}}</textarea>
+              @error('tensaubenh')  <div class="invalid-feedback"> <i class="fa fa-exclamation-circle fa-fw"></i> {{ $message }} </div>  @enderror
+            </div>
+
+              <div class="form-group">
+              <label for="dacdiem"> Đặc điểm </label>
+              <textarea class="form-control @error('dacdiem') is-invalid @enderror" id="dacdiem" name="dacdiem" rows="3" placeholder="Đặc điểm" autofocus>{{old('tensaubenh', $sauBenh->dacdiem)}}</textarea>
+              @error('dacdiem')  <div class="invalid-feedback"> <i class="fa fa-exclamation-circle fa-fw"></i> {{ $message }} </div>  @enderror
+            </div>
+
+            <div class="form-group">
+              <label for="caytrongtancong"> Cây trồng tấn công </label>
+              <textarea class="form-control @error('caytrongtancong') is-invalid @enderror" id="caytrongtancong" name="caytrongtancong" rows="3" placeholder="Cây trồng tấn công" autofocus>{{old('caytrongtancong', $sauBenh->caytrongtancong)}}</textarea>
+              @error('caytrongtancong')  <div class="invalid-feedback"> <i class="fa fa-exclamation-circle fa-fw"></i> {{ $message }} </div>  @enderror
+            </div>
+
+            <div class="form-group">
+              <label for="dauhieu"> Dấu hiệu </label>
+              <textarea class="form-control @error('dauhieu') is-invalid @enderror" id="dauhieu" name="caytrongtancong" rows="3" placeholder="Dấu hiệu" autofocus>{{old('dauhieu', $sauBenh->dauhieu)}}</textarea>
+              @error('dauhieu')  <div class="invalid-feedback"> <i class="fa fa-exclamation-circle fa-fw"></i> {{ $message }} </div>  @enderror
+            </div>
+
+            <div class="form-group">
+              <label for="bienphapphongtranh"> Biện pháp phòng tránh </label>
+              <textarea class="form-control @error('caytrongtancong') is-invalid @enderror" id="bienphapphongtranh" name="bienphapphongtranh" rows="3" placeholder="Biện pháp phòng tránh" autofocus>{{old('bienphapphongtranh', $sauBenh->bienphapphongtranh)}}</textarea>
+              @error('bienphapphongtranh')  <div class="invalid-feedback"> <i class="fa fa-exclamation-circle fa-fw"></i> {{ $message }} </div>  @enderror
+            </div>
+
+            <div class="form-group">
+              <label for="ghichu"> Ghi chú </label>
+              <textarea class="form-control @error('ghichu') is-invalid @enderror" id="ghichu" name="ghichu" rows="3" placeholder="Ghi chú" autofocus>{{old('ghichu', $sauBenh->ghichu)}}</textarea>
+              @error('ghichu')  <div class="invalid-feedback"> <i class="fa fa-exclamation-circle fa-fw"></i> {{ $message }} </div>  @enderror
+            </div>
+
+            <div class="text-center">
+              <button class="btn btn-primary" type="submit"> <i class="fa fa-save"></i> Lưu </button>
+            </div>
+          </fieldset>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
+
+@endsection
